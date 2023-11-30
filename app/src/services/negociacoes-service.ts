4dd324158b2ba8ce832a4eae3767bbb9f3d534c7
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-11-30 09:27:29 -0300

    Camada de serviços

---
+++ app/src/services/negociacoes-service.ts
@@ -0,0 +1,18 @@
+import { NegociacoesDoDia } from "../interfaces/negociacao-do-dia.js";
+import { Negociacao } from "../models/negociacao";
+
+export class NegociacoesService {
+  public async obterNegociacoesDoDia(): Promise<Negociacao[]> {
+    return fetch("http://localhost:8080/dados")
+      .then((res) => res.json())
+      .then((dados: NegociacoesDoDia[]) => {
+        return dados.map((dadoDeHoje) => {
+          return new Negociacao(
+            new Date(),
+            dadoDeHoje.vezes,
+            dadoDeHoje.montante
+          );
+        });
+      });
+  }
+}
