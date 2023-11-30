f4464b21a77b06b56fad244857dc094fe26cfd42
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-11-30 10:47:53 -0300

    Interface com Generics

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from "../interfaces/comparavel.js";
 import { Imprimivel } from "../utils/imprimivel.js";
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes implements Imprimivel {
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
   private negociacoes: Negociacao[] = [];
 
   public adiciona(negociacao: Negociacao) {
@@ -11,7 +12,14 @@ export class Negociacoes implements Imprimivel {
   public lista(): readonly Negociacao[] {
     return this.negociacoes;
   }
+
   public paraTexto(): string {
     return JSON.stringify(this.negociacoes, null, 2);
+  }
+
+  public eIgual(negociacoes: Negociacoes): boolean {
+    return (
+      JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista())
+    );
   }
 }
